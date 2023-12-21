1e9d77b9facc9b4937d125fc37f0191df48151d0
Armine Azizyan <armineazizyann@gmail.com>
2023-12-21 15:40:35 +0400

    remove comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,11 +7,9 @@ module.exports = defineConfig({
       // implement node event listeners here
       on("task", {
         setMyUser: (param1) => {
-          // Perform some logic with the parameters
           return (result = param1);
         },
         getMyUser: () => {
-          // Perform some logic with the parameters
           return result;
         },
         resetUserData: () => {
